c7c64b9cda90adc545522f017763ab7b43c90a83
bomboze@gmail.com <bomboze@gmail.com>
2010-11-23 02:25:39 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ChromeRemoteShell',
+      version='0.1',
+      description='ChromeRemoteShell',
+      author='Slava V',
+      author_email='bomboze@gmail.com',
+      py_modules = ['ChromeRemoteShell',]
+)
